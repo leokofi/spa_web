2d0f92bba3db0cf856c2e79736fc44c7cb3e84a5
Leonard Fiadzinu <leo@gmgltd.com>
2017-10-25 22:14:53 -0400

    added app mit, chobits

--- js/chobits.08-app.js
+++ js/chobits.08-app.js
@@ -49,7 +49,7 @@
         '<p><strong>hi_score</strong> appears to be properly installed!</p>'
       + '<p>The hi_score application map (app_map) attributes are as '
       + 'follows: <br>' + Object.keys( app_map ).join('<br>')
-      + '</p><p>Drag the title bar to move this lightbox.</p>'
+      + '</p><p>Drag the title bar to move this lightbox. Leo.. Leo... Leo...</p>'
     });
   }
 
--- js/chobits.08_app-build.js
+++ js/chobits.08_app-build.js
@@ -17,11 +17,11 @@ $(function () {
   var app_map = xhi._makeAppFn_( 'ex02' );
   ex02._07_shell_._initModuleFn_( $('body') );
   ex02._06_lb_._showLbFn_({
-    _title_html_ : 'Example 02',
+    _title_html_ : 'Chobits - valuations Analysitics Research',
     _content_html_ :
       '<p><strong>hi_score</strong> appears to be properly installed!</p>'
       + '<p>The hi_score application map (app_map) attributes are as '
       + 'follows: <br>' + Object.keys( app_map ).join('<br>')
-      + '</p><p>Drag the title bar to move this lightbox.</p>'
+      + '</p><p>Drag the title bar to move this lightbox. Nyah... Sonia.. Xavier</p>'
   });
 });
